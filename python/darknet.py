e083b3ab767a560e1b1fa79847143f750a47e38e
Cyan-离 <115965270+LemonFan-maker@users.noreply.github.com>
2022-11-07 23:29:55 +0800

    Update darknet.py

--- python/darknet.py
+++ python/darknet.py
@@ -44,7 +44,7 @@ class METADATA(Structure):
 
     
 
-lib = CDLL("/home/runner/darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/runner/ocr/darknet/libdarknet.so", RTLD_GLOBAL)
 lib = CDLL("libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
